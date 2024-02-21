758840f71f85f2f8dbb369dc9a150c67f05c7294
Xin shen <363088847@qq.com>
2024-02-21 23:41:40 +0800

    Add user registration functionality and report entity***

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  //获取注册信息
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
